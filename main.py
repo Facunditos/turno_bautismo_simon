3250b7ddf76e767e2b36f7ad2add32335694d195
Facunditos <facundolopezcrespo302@gmail.com>
2025-05-09 12:16:39 -0300

    retoques

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ chrome_options.add_argument('--disable-gpu')
 service = Service('/usr/bin/chromedriver')
 
 mariana_num = os.environ["MARIANA_NUM"]
-n_busqueda = 0
+# Se arranca en -1 porque la primera búsqueda no cuenta. 
+n_busqueda = -1
 
 def enviar_msg(mensaje):
     while True:
@@ -38,35 +39,41 @@ def buscar_turno():
     driver = webdriver.Chrome(options=chrome_options)
     try:
         driver.get(url)
-        # Esperar hasta que los comentarios estén visibles en la página
+        # Esperar hasta que los comentarios estén visibles en la página. Si el elemento no se encuenta, se ejecuta el bloque except
         tag_name_center = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.TAG_NAME, "center"))
         )
         texto_tag_name_center = tag_name_center.text
         msg_sin_turnos = 'En este momento la parroquia no cuenta con cupos para bautismos.\nDisculpe las molestias.'
         if texto_tag_name_center != msg_sin_turnos :
-            msg = f'Es muy probable que esté habilitada la reserva de turnos!\n{url}'
-            enviar_msg(msg)
+            msg = f'Es muy probable que esté habilitada la reserva de turnos! \U0001F600\n{url}'
+            for _ in range(2):
+                enviar_msg(msg)
+                sleep(30)
         else:
             marca_tiempo = str(datetime.now()).split('.')[0]
             print(f'hora actual: {marca_tiempo}. Volveremos a probar dentro de quince minutos')          
             global n_busqueda
             n_busqueda +=1
             if n_busqueda == 8: # Pasaron dos horas (15 min * 8) de ejecución del script 
-                msg = 'No encontramos turnos para el bautismo de Simón. Lo seguiremos intentando'
+                msg = 'No encontramos turnos para el bautismo de Simón. Lo seguiremos intentando! \U0001F4AA'
                 enviar_msg(msg)
                 n_busqueda = 0
                 
     except Exception as e:
-        msg = f'Por algún motivo no pudimos consultar la disponibilidad de turnos. Te recomendamos que visitas la página.\n{url}'
-        enviar_msg(msg)
-        # Cerrar el navegador
+        msg = f'Por algún motivo no pudimos consultar la disponibilidad de turnos \U0001F914. Te recomendamos que visitas la página.\n{url}'
+        for _ in range(2):
+            enviar_msg(msg)
+            sleep(30)
         print(f"Ocurrió un error al visitar la página: {e}.")
     
+    # Cerrar el navegador
     driver.quit()    
 
 
 def main():
+    msg = 'Buen día Mariana! Hoy nos pondremos a buscar un turno para el bautismo de Simón. Crucemos los dedos! \U0001F91E'
+    enviar_msg(msg)
     while True:
         buscar_turno()
         quince_minutos = 60 * 15
